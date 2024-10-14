6f3e0257e7c5c0f42e87a3eac6a3404bdf3b9c44
mahari9 <merytseg77@gmail.com>
2024-10-14 11:51:56 +0300

    Add BuildingModal for displaying detailed information about a building on the dashboard

---
+++ client-side/src/pages/Dashboard/BuildingModal.jsx
@@ -0,0 +1,60 @@
+import { useDashboardContext } from "../../context/dashboardContext";
+import { useGetAllPosts } from "../../hooks/useGetPosts";
+import Spinner from "../../ui/Spinner/Spinner";
+import styles from "./DashBoard.module.scss";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+export default function BuildingModal() {
+  const { activeBuilding, setActiveBuilding } = useDashboardContext();
+  const { data: allPosts, isLoading } = useGetAllPosts();
+  const coordsStr = activeBuilding && activeBuilding.join(",");
+  const navigate = useNavigate();
+
+  if (isLoading) return <Spinner />;
+  const post =
+    activeBuilding &&
+    allPosts?.posts.find((post) => post.coords[0] === coordsStr);
+
+  return (
+    <div
+      className={`${styles.buildingModal} ${
+        activeBuilding ? styles.showModal : ""
+      }`}
+    >
+      {post && (
+        <>
+          <button
+            className={styles.closeModal}
+            onClick={() => setActiveBuilding(null)}
+          >
+            <AiFillCloseCircle className={styles.iconClose}/>
+          </button>
+          <img src={`/${post.imgs[0]}`} />
+          <div className={styles.generalInfo}>
+            <span>{post.typeProperty}</span>
+            <span>{post.listingPurpose}</span>
+            <span>
+              <img src="/location.svg" />
+              {post.location}
+            </span>
+          </div>
+
+          <div className={styles.info}>
+            <h2>{post.title}</h2>
+            <span className={styles.subtitle}>{[post.subtitle]}</span>
+            <span className={styles.price}>
+              {post.price.toLocaleString("en-US")} Birr
+            </span>
+            <span className={styles.description}>{post.description}</span>
+          </div>
+          <button
+            className={styles.buttonAdvanced}
+            onClick={() => navigate(`/app/post/${post._id}`)}
+          >
+            Details
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
