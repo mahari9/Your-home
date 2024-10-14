8e90948e7bc565a7c222d9524162a47f0326b726
mahari9 <merytseg77@gmail.com>
2024-10-14 11:06:52 +0300

    Create LandingPage component with navigation and loading spinner

---
+++ client-side/src/pages/Landingpage/LandingPage.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import { usePreventAccess } from "../../hooks/usePreventAccess";
+import Spinner from "../../ui/Spinner/Spinner";
+import styles from "./LandingPage.module.scss";
+
+export default function LandingPage() {
+  const navigate = useNavigate();
+  const isLoading = usePreventAccess();
+  if (isLoading) return <Spinner />;
+  return (
+    <div className={styles.landingPage}>
+      {/* <h1>Your Home</h1>*/}
+      <img src="/logo.svg" />
+      <h2>Your perfect property, within reach.</h2>
+      <div>
+        <button onClick={() => navigate("/app")}>Look at the listing</button>
+        <button onClick={() => navigate("/register")}>Register</button>
+      </div>
+    </div>
+  );
+}
