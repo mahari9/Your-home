99fd201e76bebdf0520c0f63fbd86a8c1300e8ae
mahari9 <merytseg77@gmail.com>
2024-10-14 17:48:53 +0300

    Add ImageComponent for displaying post images

---
+++ client-side/src/pages/Post/ImageComponent.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import Carousel from "../../ui/Slider/Slider";
+import styles from "./ImageComponent.module.scss";
+export default function ImageComponent({ post }) {
+  return (
+    <div className={styles.imageWrapper}>
+      <Carousel>
+        {post.imgs.map((image, i) => (
+          <img src={`/${image}`} key={i} alt="Real Estate Image" />
+        ))}
+      </Carousel>
+    </div>
+  );
+}
