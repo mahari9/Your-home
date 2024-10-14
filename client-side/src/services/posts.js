43c0380c2b59ee0d57da792fb7432b786e836868
mahari9 <merytseg77@gmail.com>
2024-10-14 18:46:30 +0300

    Update post service functions for fetching, deleting, and finishing posts

---
+++ client-side/src/services/posts.js
@@ -0,0 +1,46 @@
+const BASE_URL = `/`;
+export async function getPosts() {
+  const queryString = window.location.search;
+  queryString.replace("All", "");
+  try {
+    const res = await fetch(`${BASE_URL}api/posts${queryString}`);
+    const data = res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export async function getSinglePost(postId) {
+  try {
+    const res = await fetch(`${BASE_URL}api/posts/${postId}`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export async function deletePost(postId) {
+  try {
+    const res = await fetch(`${BASE_URL}api/posts/${postId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export async function finishPost(postId) {
+  try {
+    const res = await fetch(`${BASE_URL}api/posts/post/finish/${postId}`, {
+      method: "PATCH",
+    });
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
