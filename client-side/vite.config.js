24523a7479b7239ad3d4d0d63392c0cb4e149e5f
mahari9 <merytseg77@gmail.com>
2024-10-14 09:39:48 +0300

    Update client-side package, ESLint configuration, vite configuration and gitignore

---
+++ client-side/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    cssCodeSplit: true, // Enable CSS code splitting
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+});
