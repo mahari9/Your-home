b208582e61090ecbafef7444c2fd2ca782580d9a
mahari9 <merytseg77@gmail.com>
2024-10-15 22:40:33 +0300

    Add Footer component to AppLayout

--- client-side/src/ui/AppLayout/AppLayout.jsx
+++ client-side/src/ui/AppLayout/AppLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function AppLayout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
